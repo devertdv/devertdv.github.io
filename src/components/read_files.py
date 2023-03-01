919e5ae96365941ae5a82c1740f4b238578a0176
Darko Vrtsel <devert.dv@gmail.com>
2023-03-01 04:48:55 +0300

    added changes

--- src/components/read_files.py
+++ src/components/read_files.py
@@ -1,29 +1,29 @@
 import pandas as pd
 
-df_authors = pd.read_csv('data/authors_rate.csv', header=None, sep=",")
+df_authors = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/authors_rate.csv', header=None, sep=",")
 df_authors.columns = ["Author", "Rate"]
 df_authors.sort_values("Rate", ascending=True)
 
-df_organizations = pd.read_csv('data/organizations_rate.csv', header=None, sep=",")
+df_organizations = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/organizations_rate.csv', header=None, sep=",")
 df_organizations.columns = ["Organization", "Rate"]
 df_organizations.sort_values("Rate", ascending=True)
 
-df_fundings = pd.read_csv('data/funding_rate.csv', header=None, sep=",")
+df_fundings = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/funding_rate.csv', header=None, sep=",")
 df_fundings.columns = ["Funding", "Rate"]
 df_fundings.sort_values("Rate", ascending=True)
 
-df_countries = pd.read_csv('data/countries_rate.csv', header=None, sep=",")
+df_countries = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/countries_rate.csv', header=None, sep=",")
 df_countries.columns = ["Country", "Rate"]
 df_countries.sort_values("Rate", ascending=True)
 
-df_sources = pd.read_csv('data/sources_rate.csv', header=None, sep=",")
+df_sources = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/sources_rate.csv', header=None, sep=",")
 df_sources.columns = ["Source", "Rate"]
 df_sources.sort_values("Rate", ascending=True)
 
-df_dinamic = pd.read_csv('data/publications_dinamic.csv', header=None, sep=",")
+df_dinamic = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/publications_dinamic.csv', header=None, sep=",")
 df_dinamic.columns = ["Year", "Number"]
 
-df_big_table = pd.read_csv('data/top-2000_pub_by citation.csv', sep=',')
+df_big_table = pd.read_csv('https://raw.githubusercontent.com/devertdv/devertdv.github.io/main/data/top-2000_pub_by%20relevance.csv', sep=',')
 df_big_table["Author Keywords"] = df_big_table["Author Keywords"].fillna('')
 df_big_table["Author Keywords"] = df_big_table["Author Keywords"].apply(str.lower)
 df_big_table["Year"] = df_big_table["Year"].astype('object')
